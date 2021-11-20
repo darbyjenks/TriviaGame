96722a67884d7cd419ff6a3bc279865a711f9184
Darby Johnson <darbyjjohnson1@gmail.com>
2021-11-20 12:16:25 -0500

    created basic structure containers

---
+++ script.js
@@ -0,0 +1,6 @@
+let quizContainer=document.getElementById(quiz);
+let resultsContainer=document.getElementById(results);
+let buttonContainer=document.getElementById(button);
+let resultsContainer=document.getElementById(results);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton)
